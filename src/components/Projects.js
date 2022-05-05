d2745b39964e2e57a4bb065e5cd06e3524c8e2fe
NELSON ANDRES LUENGAS SILVA <90053867+NELSONLUENGAS@users.noreply.github.com>
2022-05-05 13:06:46 -0500

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -33,7 +33,7 @@ export default function Projects(){
                 <button onClick={()=> window.location.href = 'https://github.com/NELSONLUENGAS/PORTAFOLIO'}>
                     <GitHubIcon/>
                 </button>
-                <button onClick={()=> window.location.href = 'https://portafolio-6mdiwit0b-nelsonluengas.vercel.app/'}>
+                <button onClick={()=> window.location.href = 'https://portafolio-a3arclg24-nelsonluengas.vercel.app'}>
                     <LanguageIcon/>
                 </button>
                 </div>
