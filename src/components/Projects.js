0ae321fdc8a78607eed50f5aeaaab3102a182777
NELSON ANDRES LUENGAS SILVA <90053867+NELSONLUENGAS@users.noreply.github.com>
2022-05-05 12:30:58 -0500

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import image from '../images/2.jpg';
+import image1 from '../images/image.png';
+import image2 from '../images/image0.png';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LanguageIcon from '@mui/icons-material/Language';
 import '../scss/components/Projects.scss';
 
-export default function Projects(){
+export default function Projects({id}){
     return(
-        <div className='projectGrid' id='projects'>
+        <div className='projectGrid' id={id}>
             <div className='projectContainer'>
                 <div className='project'>
                 <img src={image} alt='icono1'/>
@@ -18,38 +20,20 @@ export default function Projects(){
                 </button>
                 </div>
                 <div className='project'>
-                <img src={image} alt='icono1'/>
-                <button>
+                <img src={image1} alt='icono1'/>
+                <button onClick={()=> window.location.href = 'https://github.com/NELSONLUENGAS/E-Comerce_ProyectoFinal'}>
                     <GitHubIcon/>
                 </button>
-                <button>
+                <button onClick={()=> window.location.href = 'https://ecommerce-app-fawn.vercel.app'}>
                     <LanguageIcon/>
                 </button>
                 </div>
                 <div className='project'>
-                <img src={image} alt='icono1'/>
-                <button>
+                <img src={image2} alt='icono1'/>
+                <button onClick={()=> window.location.href = 'https://github.com/NELSONLUENGAS/PORTAFOLIO'}>
                     <GitHubIcon/>
                 </button>
-                <button>
-                    <LanguageIcon/>
-                </button>
-                </div>
-                <div className='project'>
-                <img src={image} alt='icono1'/>
-                <button>
-                    <GitHubIcon/>
-                </button>
-                <button>
-                    <LanguageIcon/>
-                </button>
-                </div>
-                <div className='project'>
-                <img src={image} alt='icono1'/>
-                <button>
-                    <GitHubIcon/>
-                </button>
-                <button>
+                <button onClick={()=> window.location.href = 'https://portafolio-6mdiwit0b-nelsonluengas.vercel.app/'}>
                     <LanguageIcon/>
                 </button>
                 </div>
