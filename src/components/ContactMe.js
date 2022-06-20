e502af58c003fbf56546088b852e1a25429a1d7b
NELSON ANDRES LUENGAS SILVA <90053867+NELSONLUENGAS@users.noreply.github.com>
2022-06-20 08:17:26 -0500

    contact

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -8,45 +8,45 @@ import '../scss/components/ContactMe.scss';
 export default function ContactMe(){
     return(
         <div className='contactGrid' id='contact'>
-        <div className='contactContainer'>
-        <div className='contactElement1'>
-            <h2>Contact Me¡¡ 😎</h2>
+            <div className='contactContainer'>
+                <div className='contactElement1'>
+                    <h2>Contact Me!! 😎</h2>
+                </div>
+                <div className='contactForm'>
+                    <textarea rows='5'></textarea>
+                    <br></br>
+                    <button>Enviar</button>
+                    <br></br>
+                    <br></br>
+                </div>
+                <div className='social'>
+                <div className='whatsApp' onClick={()=> window.location.href = 'https://wa.link/1y88f3'}>
+                    <span>WhatsApp</span>
+                    <button>
+                        <WhatsAppIcon/>
+                    </button>
+                </div> 
+                <div className='gmail'>
+                    <a style={{textDecoration: 'none', color: 'white'}} href='mailto:Nelsonluengas2@gmail.com'>Gmail</a>
+                    <button>
+                        <EmailIcon/>
+                    </button>
+                </div>
+                <div className='linkedIn' onClick={()=> window.location.href = 'https://www.linkedin.com/in/nelsonluengas'}>
+                    <span>LikendIn</span>
+                    <button>
+                        <LinkedInIcon/>
+                    </button>
+                </div>
+                <div className='gitHub' onClick={()=> window.location.href = 'https://github.com/NELSONLUENGAS'}>
+                    <span>GitHub</span>
+                    <button>
+                        <GitHubIcon/>
+                    </button>
+                </div>
+                </div>
+                <div className='copyRight'> Nelson Andrés Luengas Silva - 2022   ©️ Copyright</div>
+            </div>
         </div>
-        <div className='contactForm'>
-            <textarea></textarea>
-            <br></br>
-            <button>Enviar</button>
-            <br></br>
-            <br></br>
-        </div>
-        <div className='social'>
-        <div className='whatsApp' onClick={()=> window.location.href = 'https://wa.link/1y88f3'}>
-            <span>WhatsApp</span>
-            <button>
-                <WhatsAppIcon/>
-            </button>
-        </div> 
-        <div className='gmail'>
-            <a style={{textDecoration: 'none', color: 'white'}} href='mailto:Nelsonluengas2@gmail.com'>Gmail</a>
-            <button>
-                <EmailIcon/>
-            </button>
-        </div>
-        <div className='linkedIn' onClick={()=> window.location.href = 'https://www.linkedin.com/in/nelsonluengas'}>
-            <span>LikendIn</span>
-            <button>
-                <LinkedInIcon/>
-            </button>
-        </div>
-        <div className='gitHub' onClick={()=> window.location.href = 'https://github.com/NELSONLUENGAS'}>
-            <span>GitHub</span>
-            <button>
-                <GitHubIcon/>
-            </button>
-        </div>
-        </div>
-        <div className='copyRight'> Nelson Andrés Luengas Silva - 2022   ©️ CopyRight</div>
-        </div>
-    </div>
     )
 }
